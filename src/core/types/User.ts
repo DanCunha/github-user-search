b344afc1657a4b9f7dfacbd0a2251b0fc0acca2e
Daniel Cunha <cunhacdaniel@gmail.com>
2021-05-27 13:31:13 -0400

    challenge finished

---
+++ src/core/types/User.ts
@@ -0,0 +1,21 @@
+export type User = {
+    id: number,
+    login: string,
+    avatar_url: string,
+    url: string,
+    html_url: string,
+    name: string,
+    company: string,
+    blog: string,
+    location: string,
+    email: string,
+    hireable: boolean,
+    bio: string,
+    twitter_username: string,
+    public_repos: number,
+    public_gists: number,
+    followers: number,
+    following: number,
+    created_at: string,
+    updated_at: string
+}
--- src/pages/Search/Form/index.tsx
+++ src/pages/Search/Form/index.tsx
@@ -1,17 +1,22 @@
-
 import { makeRequest } from '../../../core/utils/request';
 import React, { useState } from 'react';
 import BaseForm from '../../components/BaseForm';
 import './styles.scss';
+import { User } from '../../../core/types/User';
+import InfoLoader from '../components/Loaders/InfoLoader';
+import ImageLoader from '../components/Loaders/ImageLoader';
 
 type FormState = {
     user: string;
 }
 
+var dayjs = require('dayjs')
+
 type FormEvent = React.ChangeEvent<HTMLInputElement>;
 
 const Form = () => {
-  //  const [productsResponse, setProductsresponse] = useState<ProductsResponse>();
+    const [user, setUser] = useState<User>();
+    const [showInfo, setShowInfo] = useState(true);
     const [isLoading, setIsLoading] = useState(false);
 
     const [formData, setFormData] = useState<FormState>({
@@ -23,8 +28,6 @@ const Form = () => {
         const name = event.target.name;
         const value = event.target.value;
 
-        console.log(value);
-
         setFormData(data => ({ ...data, [name]: value }));
     }
 
@@ -32,11 +35,12 @@ const Form = () => {
         event.preventDefault();
 
         console.log("Componente Iniciado");
+        setShowInfo(false);
         setIsLoading(true);
         makeRequest({ url: `/users/${formData.user}`})
-        .then(response => console.log(response.data))
+        .then(response => setUser(response.data))
         .finally(() => {
-            setIsLoading(false);
+           setIsLoading(false);
         })
     }
 
@@ -54,6 +58,41 @@ const Form = () => {
                         />
                     </div>
             </BaseForm>
+            {showInfo ? <div /> : (
+                <>
+                    <div className="info-container">            
+                        <div className="info-img">
+                            {isLoading ? <ImageLoader /> : (
+                                <>
+                                    <img src={user?.avatar_url} alt={user?.name} className="user-details-image" />
+                                    <a href={user?.html_url} className="btn-profile">
+                                        Ver Perfil
+                                    </a>
+                                </>
+                            )}
+                        </div>
+                        <div className="info-data">
+                            {isLoading ? <InfoLoader /> : (
+                                <>
+                                    <div className="info-repo">
+                                        <p>Repositórios públicos: {user?.public_repos}</p>
+                                        <p>Seguidores: {user?.followers}</p>
+                                        <p>Seguindo: {user?.following}</p>
+                                    </div>
+                                    <div className="info-details">
+                                        <h1>Informações</h1>
+                                        <p><b>Empresa: </b>{user?.company}</p>
+                                        <p><b>Website/Blog: </b>{user?.blog}</p>
+                                        <p><b>Localidade: </b>{user?.location}</p>
+                                        <p><b>Membro desde: </b>{dayjs(user?.created_at).format('DD/MM/YYYY')}</p>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </>
+            )}
+            
         </form>
     )
 }
